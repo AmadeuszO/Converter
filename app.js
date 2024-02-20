acb9c448cc84f6af35e2418666d8bd7c603d0ef2
Amadeusz <amadi9205@gmail.com>
2024-02-20 17:34:06 +0100

    adding variables adding styles

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 const textArea = document.querySelector('textarea');
 const btn = document.querySelector('button');
+
+const textConverter = () =>{
+    const synText = window.speechSynthesis;
+    const text = textArea.value;
+
+    const utternace = new SpeechSynthesisUtterance(text);
+    synText.speak(utternace)
+}
+
+btn.addEventListener('click', textConverter);
